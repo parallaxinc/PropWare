904e941e5660e1545ca7f806920c9ed2a6ad2d7f
David Zemon <david@stlswedespeed.com>
2013-10-01 14:46:40 -0500

    New SPI interface ready for testing

--- PropGCC_Demos/SPI/SPI_Demo.c
+++ PropGCC_Demos/SPI/SPI_Demo.c
@@ -8,35 +8,35 @@
 
 // Main function
 void main (void) {
-	char string[50] = "Hello world!\n";					// Create a string constant to output over SPI
-	char *s;											// Create a pointer variable that can be incremented in a loop
-	char in;											// Create an input variable to store received values from SPI
+	char string[50] = "Hello world!\n";		// Create a string constant to output over SPI
+	char *s;				// Create a pointer variable that can be incremented in a loop
+	char in;				// Create an input variable to store received values from SPI
 
-	SPIStart(MOSI, MISO, SCLK, FREQ, POLARITY);			// Initialize SPI module, giving it pin masks for the physical pins, frequency for the clock, and polarity of the clock
-	GPIODirModeSet(CS, GPIO_DIR_OUT);					// Set chip select as an output (Note: the SPI module does not control chip select)
+	SPIStart(MOSI, MISO, SCLK, FREQ, SPI_MODE_0, SPI_MSB_FIRST);// Initialize SPI module, giving it pin masks for the physical pins, frequency for the clock, and polarity of the clock
+	GPIODirModeSet(CS, GPIO_DIR_OUT);// Set chip select as an output (Note: the SPI module does not control chip select)
 
 	while (1) {
-		s = string;										// Set the pointer to the beginning of the string
-		while (*s) {									// Loop until we read the null-terminator
+		s = string;						// Set the pointer to the beginning of the string
+		while (*s) {							// Loop until we read the null-terminator
 
 //			waitcnt(CLKFREQ/100 + CNT);
 
 			in = 0xff;									// Reset input variable
-			GPIOPinClear(CS);							// Enable the SPI slave attached to CS
-			SPIShiftOut(8, *s, SPI_MSB_FIRST);		// Output the next character of the string and increment the pointer to following character - note the "SPI_MSB_FIRST", meaning we intend to send the data out MSB-first
+			GPIOPinClear(CS);						// Enable the SPI slave attached to CS
+			SPIShiftOut(8, *s);	// Output the next character of the string and increment the pointer to following character - note the "SPI_MSB_FIRST", meaning we intend to send the data out MSB-first
 			SPIWait();
-			GPIOPinSet(CS);								// Disable the SPI slave attached to CS
+			GPIOPinSet(CS);						// Disable the SPI slave attached to CS
 
 			waitcnt(CLKFREQ/100 + CNT);
 			while (in != *s) {
 				GPIOPinClear(CS);
-				SPIShiftIn(8, SPI_MSB_PRE, &in, 1);			// Read in a value from the SPI device - note the "SPI_MSB_PRE", meaning we intend to interpret the data as MSB-first, and the data will be valid before (PRE) the clock cycle
+				SPIShiftIn(8, &in, 1);  // Read in a value from the SPI device - note the "SPI_MSB_PRE", meaning we intend to interpret the data as MSB-first, and the data will be valid before (PRE) the clock cycle
 				GPIOPinSet(CS);
 			}
 
 			++s;
 
-			putchar(in);								// Print the character to the screen
+			putchar(in);							// Print the character to the screen
 		}
 	}
 }
