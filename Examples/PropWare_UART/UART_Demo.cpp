6ddbb7e07e4117d87e046487195068a324e8399e
David Zemon <david@zemon.name>
2014-04-20 21:38:35 -0500

    Simplex working (I think) with lots of debugging lines

---
+++ Examples/PropWare_UART/UART_Demo.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file    UART_Demo.cpp
+ *
+ * @project UART_Demo
+ *
+ * @author  David Zemon
+ *
+ * @copyright
+ * The MIT License (MIT)<br>
+ * <br>Copyright (c) 2013 David Zemon<br>
+ * <br>Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:<br>
+ * <br>The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.<br>
+ * <br>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "UART_Demo.h"
+
+/**
+ * @brief   Write "Hello world!\n" out via SPI protocol and receive an echo
+ */
+int main () {
+    char string[] = "Hello world!\n";  // Create the test string
+    char *s;    // Create a pointer variable that can be incremented in a loop
+    PropWare::SimplexUART uart(TX);
+    PropWare::SimplePort debugLEDs(PropWare::Port::P16, 8, PropWare::Pin::OUT);
+
+    uart.set_baud_rate(BAUD);
+    uart.set_data_width(8);
+    uart.set_stop_bit_width(1);
+    uart.set_parity(PropWare::UART::NO_PARITY);
+
+    printf("Stop bit mask: 0x%04x\n", uart.m_stopBitMask);
+    printf("Data mask: 0x%04x\n", uart.m_dataMask);
+    printf("Total bits: %u\n", uart.m_totalBits);
+
+    while (1) {
+        s = string;         // Set the pointer to the beginning of the string
+        while (*s) {        // Loop until we read the null-terminator
+            uart.send(*s);  // Output the next character of the string
+
+            waitcnt(CLKFREQ/4 + CNT);
+
+            // Increment the character pointer
+            ++s;
+        }
+
+        // Signal that the entire string has been sent
+//        debugLEDs.toggle();
+    }
+
+    return 0;
+}
+
+void error (const PropWare::ErrorCode err) {
+    PropWare::SimplePort debugLEDs(PropWare::Port::P16, 8, PropWare::Pin::OUT);
+
+    printf("Unknown error %u\n", err);
+
+    while (1) {
+        debugLEDs.write(err);
+        waitcnt(100*MILLISECOND);
+        debugLEDs.write(0);
+        waitcnt(100*MILLISECOND);
+    }
+}
--- PropWare/uart.h
+++ PropWare/uart.h
@@ -29,7 +29,11 @@
 #define PROPWARE_UART_H_
 
 #include <sys/thread.h>
+#include <tinyio.h> // TODO: DEBUG LINE; delete
+#include <simpletext.h>
 #include <PropWare/PropWare.h>
+#include <PropWare/pin.h>
+#include <PropWare/port.h>
 
 namespace PropWare {
 
@@ -40,99 +44,195 @@ class UART {
 
     public:
         typedef enum {
-            /** 5 Bits */B5,
-            /** 6 Bits */B6,
-            /** 7 Bits */B7,
-            /** 8 Bits */B8,
-            /** 9 Bits */B9
-        } DataWidth;
-
-        typedef enum {
             /** No parity */NO_PARITY,
             /** Even parity */EVEN,
             /** Odd parity */ODD
         } Parity;
 
+        /** Number of allocated error codes for UART */
+#define UART_ERRORS_LIMIT            16
+        /** First UART error code */
+#define UART_ERRORS_BASE             64
+
+        /**
+         * Error codes - Proceeded by SD, SPI
+         */
         typedef enum {
-            NO_ERROR = 0
+            NO_ERROR = 0,
+            BEG_ERROR = UART_ERRORS_BASE,
+            BAUD_TOO_HIGH = BEG_ERROR,
+            INVALID_STOP_BIT_WIDTH,
+            END_ERROR = PropWare::UART::INVALID_STOP_BIT_WIDTH
         } ErrorCode;
 
-#ifndef PROPWARE_NO_SAFE_UART
-    private:
-#else
     public:
-#endif
-        UART () {
-            this->m_mailbox = -1;
-            this->m_cog = -1;
-        }
+        static const uint8_t DEFAULT_DATA_WIDTH = 8;
+        static const PropWare::UART::Parity DEFAULT_PARITY = NO_PARITY;
+        static const uint8_t DEFAULT_STOP_BIT_WIDTH = 1;
+        static const uint32_t DEFAULT_BAUD = 115200;
+
+        static const uint32_t MAX_BAUD = -1;
 
     public:
-        static UART* getInstance () {
-            // TODO: Add a lock to make this thread-safe
-            static UART instance;
-            return &instance;
+        void set_data_width (const uint8_t dataWidth) {
+            this->m_dataWidth = dataWidth;
+
+            this->m_dataMask = 0;
+            for (uint8_t i = 0; i < this->m_dataWidth; ++i)
+                this->m_dataMask |= 1 << i;
+
+            this->set_total_bits();
+            this->set_parity_mask();
         }
 
-        static atomic_t s;
-
-        PropWare::ErrorCode start (const PropWare::Port::Mask tx,
-                const PropWare::Port::Mask rx, const int32_t baud,
-                const UART::DataWidth dataWidth, const UART::Parity parity,
-                const uint8_t stopBits);
-
-        PropWare::ErrorCode stop () {
-            if (!this->is_running())
-                return UART::NO_ERROR;
-
-            cogstop(this->m_cog);
-            this->m_cog = -1;
-            this->m_mailbox = -1;
-
-            return UART::NO_ERROR;
+        uint8_t get_set_data_width () const {
+            return this->m_dataWidth;
         }
 
-        bool is_running () {
-            return -1 != this->m_cog;
+        void set_parity (const PropWare::UART::Parity parity) {
+            this->m_parity = parity;
+            this->set_total_bits();
+            this->set_parity_mask();
         }
 
-        PropWare::ErrorCode wait () {
-            const uint32_t timeoutCnt = UART::WR_TIMEOUT_VAL + CNT;
-
-            // Wait for GAS cog to read in value and write -1
-            while ((uint32_t) -1 != this->m_mailbox)
-                if (abs(timeoutCnt - CNT) < UART::TIMEOUT_WIGGLE_ROOM)
-                    return UART::TIMEOUT;
-
-            return UART::NO_ERROR;
+        PropWare::UART::Parity get_parity () const {
+            return this->m_parity;
         }
 
-        PropWare::ErrorCode wait_specific (const uint32_t value) {
-            const uint32_t timeoutCnt = UART::WR_TIMEOUT_VAL + CNT;
+        PropWare::ErrorCode set_stop_bit_width (const uint8_t stopBitWidth) {
+            // Error checking
+            if (0 == stopBitWidth)
+                return PropWare::UART::INVALID_STOP_BIT_WIDTH;
 
-            // Wait for GAS cog to read in value and write -1
-            while (value == this->m_mailbox)
-                if (abs(timeoutCnt - CNT) < UART::TIMEOUT_WIGGLE_ROOM)
-                    // Always use return instead of spi_error() for private
-                    // f0unctions
-                    return UART::TIMEOUT;
+            this->m_stopBitWidth = stopBitWidth;
 
-            return UART::NO_ERROR;
+            this->set_stop_bit_mask();
+
+            this->set_total_bits();
+
+            return NO_ERROR;
         }
 
-        PropWare::ErrorCode set_bit_width (const uint8_t dataWidth);
+        uint8_t get_stop_bit_width () const {
+            return this->m_stopBitWidth;
+        }
 
-        PropWare::ErrorCode set_parity (const PropWare::UART::Parity parity);
+        PropWare::ErrorCode set_baud_rate (const uint32_t baudRate) {
+            if (PropWare::UART::MAX_BAUD < baudRate)
+                return PropWare::UART::BAUD_TOO_HIGH;
 
-        PropWare::ErrorCode set_stop_bit_width (const uint8_t stopBitWidth);
+            this->m_bitCycles = CLKFREQ / baudRate;
+            return NO_ERROR;
+        }
 
-        PropWare::ErrorCode set_baud_rate (const uint32_t baudRate);
+        uint32_t get_baud_rate () const {
+            return CLKFREQ / this->m_bitCycles;
+        }
 
-        PropWare::ErrorCode send (const uint16_t data);
+        HUBTEXT void send (uint16_t data) {
+            printf("Original data: 0x%04x, %u\n", data, data);
+            putBin(data);
+            printf("\n");
+
+            // Add parity bit
+            this->m_parityMask = 1 << this->m_dataWidth;
+            if (PropWare::UART::EVEN == this->m_parity) {
+                __asm__ volatile("test %[_data], %[_dataMask] wc \n\t"
+                        "muxc %[_data], %[_parityMask]"
+                        : [_data] "+r" (data)
+                        : [_dataMask] "r" (this->m_dataMask),
+                          [_parityMask] "r" (this->m_parityMask));
+            } else if (PropWare::UART::ODD == this->m_parity) {
+                __asm__ volatile("test %[_data], %[_dataMask] wc \n\t"
+                        "muxnc %[_data], %[_parityMask]"
+                        : [_data] "+r" (data)
+                        : [_dataMask] "r" (this->m_dataMask),
+                          [_parityMask] "r" (this->m_parityMask));
+            }
+
+            printf("Parity mask: 0x%04x\n", this->m_parityMask);
+            printf("Data after parity: 0x%04x\n", data);
+
+            // Add stop bits
+            data |= this->m_stopBitMask;
+
+            printf("Data after stop bits: 0x%04x\n", data);
+
+            // Add start bit
+            data <<= 1;
+
+            printf("Final data: 0x%04x\n", data);
+            printf("                    ");
+            putBin(data);
+            printf("\n");
+
+            uint32_t waitCycles = CNT + this->m_bitCycles;
+            for (uint8_t i = 0; i < this->m_totalBits; i++) {
+                waitCycles = waitcnt2(waitCycles, this->m_bitCycles);
+
+                // if (value & 1) OUTA |= this->m_tx else OUTA &= ~this->m_tx; value = value >> 1;
+                __asm__ volatile("shr %[_data],#1 wc \n\t"
+                        "muxc outa, %[_mask]"
+                        : [_data] "+r" (data)
+                        : [_mask] "r" (this->m_tx.get_mask()));
+//                printf("Data after sending: ");
+//                putBin(data);
+//                printf("\n");
+            }
+        }
 
     protected:
-        atomic_t m_mailbox;
-        uint8_t m_cog;
+        UART () {
+            this->m_dataWidth = UART::DEFAULT_DATA_WIDTH;
+            this->m_parity = UART::DEFAULT_PARITY;
+            this->set_stop_bit_width(UART::DEFAULT_STOP_BIT_WIDTH);
+            this->set_baud_rate(UART::DEFAULT_BAUD);
+        }
+
+        void set_stop_bit_mask () {
+            // Create the mask to the far right
+            this->m_stopBitMask = 1;
+            for (uint8_t i = 0; i < this->m_stopBitWidth - 1; ++i)
+                this->m_stopBitMask |= this->m_stopBitMask << 1;
+
+            // Shift the mask into position (taking into account the current
+            // parity settings)
+            this->m_stopBitMask <<= this->m_dataWidth;
+            if (PropWare::UART::NO_PARITY != this->m_parity)
+                this->m_stopBitMask <<= 1;
+        }
+
+        void set_total_bits () {
+            // Total bits = start + data + parity + stop bits
+            this->m_totalBits = 1 + this->m_dataWidth + this->m_stopBitWidth;
+            if (PropWare::UART::NO_PARITY != this->m_parity)
+                ++this->m_totalBits;
+        }
+
+        void set_parity_mask () {
+            this->m_parityMask = 1 << this->m_dataWidth;
+        }
+
+    public: // TODO: DEBUG LINE; change to 'protected'
+        PropWare::Pin m_tx;
+        uint8_t m_dataWidth;
+        uint16_t m_dataMask;
+        PropWare::UART::Parity m_parity;
+        uint16_t m_parityMask;
+        uint8_t m_stopBitWidth;
+        uint32_t m_stopBitMask;  // Does not take into account parity bit!
+        uint32_t m_bitCycles;
+        uint8_t m_totalBits;
+};
+
+class SimplexUART: public PropWare::UART {
+    public:
+        SimplexUART (const PropWare::Port::Mask tx) :
+                PropWare::UART() {
+            this->m_tx.set_mask(tx);
+            this->m_tx.set_dir(PropWare::Port::OUT);
+            this->m_tx.set();
+        }
 };
 
 }
