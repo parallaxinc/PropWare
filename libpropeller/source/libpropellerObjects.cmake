fb4a976f1b3009f5b30c03ab8b9706adf62cbd8d
David Zemon <david@zemon.name>
2014-05-03 23:12:19 -0500

    forgot another file

---
+++ libpropeller/source/libpropellerObjects.cmake
@@ -0,0 +1,8 @@
+set(LIBPROPELLER_OBJECTS
+        ../full_duplex_serial.S
+        ../mcp3208.S
+        ../numbers.cpp
+        ../pulse_width_reader.S
+        ../pwm2.S
+        ../quadrature_encoder.S
+        ../serial.S)
