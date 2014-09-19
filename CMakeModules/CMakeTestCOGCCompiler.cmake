a76200d41ed460f21621b41a94f8b313780d86ac
David Zemon <david@zemon.name>
2014-09-19 07:09:30 -0500

    Push CMake fix

---
+++ CMakeModules/CMakeTestCOGCCompiler.cmake
@@ -0,0 +1,5 @@
+
+# The compiler configuration was forced by the user.
+# Assume the user has configured all compiler information.
+set(CMAKE_COGC_COMPILER_WORKS TRUE)
+return()
---
+++ CMakeModules/CMakeTestECOGCXXCompiler.cmake
@@ -0,0 +1,5 @@
+
+# The compiler configuration was forced by the user.
+# Assume the user has configured all compiler information.
+set(CMAKE_ECOGCXX_COMPILER_WORKS TRUE)
+return()
