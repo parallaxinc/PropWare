cc8b82e44e37eb053c31d992e3438e9619195196
David Zemon <david@zemon.name>
2015-03-13 21:23:21 -0500

    Create GettingStarted example

---
+++ Examples/GettingStarted/GettingStarted.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file    GettingStarted.cpp
+ *
+ * @author  David Zemon
+ *
+ * @copyright
+ * The MIT License (MIT)<br>
+ * <br>Copyright (c) 2013 David Zemon<br>
+ * <br>Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:<br>
+ * <br>The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.<br>
+ * <br>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <PropWare/printer/printer.h>
+
+int main () {
+    pwOut << "##############################\n";
+    pwOut << "### Welcome to PropWare!!! ###\n";
+    pwOut << "##############################\n";
+    pwOut << '\n';
+    pwOut << "Congratulations on getting your first PropWare application running!\n";
+    pwOut << "Now that you know how to compile and debug a program, try \n"
+            << "writing your own \"Hello, world!\" application.\n";
+    pwOut << '\n';
+    pwOut << "Good bye!\n\n";
+
+    return 0;
+}
+
