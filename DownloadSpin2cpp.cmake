cccd71eb334233fc1c8a2078cc823d1ce28f1d7d
David Zemon <david@zemon.name>
2016-03-15 21:33:46 -0500

    [#80] Do not download CMake binaries by default; Enable per-OS and per-architecture builds

--- DownloadSpin2cpp.cmake
+++ DownloadSpin2cpp.cmake
@@ -1,8 +1,10 @@
-externalproject_add(Spin2Cpp
-    PREFIX Spin2Cpp
-    BINARY_DIR Spin2Cpp/src/Spin2Cpp
-    GIT_REPOSITORY https://github.com/totalspectrum/spin2cpp.git
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E echo_append
-    BUILD_COMMAND make && make CROSS=win32
-    INSTALL_COMMAND ${CMAKE_COMMAND} -E echo_append
-    )
+if (LINUX_PACKAGE OR WIN32_PACKAGE OR PI2_PACKAGE)
+    externalproject_add(Spin2Cpp
+        PREFIX Spin2Cpp
+        BINARY_DIR Spin2Cpp/src/Spin2Cpp
+        GIT_REPOSITORY https://github.com/totalspectrum/spin2cpp.git
+        CONFIGURE_COMMAND ${CMAKE_COMMAND} -E echo_append
+        BUILD_COMMAND make && make CROSS=win32 && make CROSS=rpi
+        INSTALL_COMMAND ${CMAKE_COMMAND} -E echo_append
+        )
+endif ()
