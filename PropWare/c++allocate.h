d1ead4c6cdae6a10571a1bf7dce5217e6aeba0a1
David Zemon <david@zemon.name>
2016-03-22 18:04:35 -0500

    [Fixes #81] Add two-parameter `operator delete` for GCC5+ compatibility

--- PropWare/c++allocate.h
+++ PropWare/c++allocate.h
@@ -3,49 +3,49 @@
 #include <stdlib.h>
 #include <new>
 
-using std::new_handler;
-new_handler __new_handler;
+std::new_handler __new_handler;
 
 void *
-operator new (std::size_t sz)
-{
-void *p;
+operator new (std::size_t sz) {
+    void *p;
 
 /* malloc (0) is unpredictable; avoid it.  */
-if (sz == 0)
-  sz = 1;
-p = (void *) malloc (sz);
-while (p == 0)
-  {
-    new_handler handler = __new_handler;
-    if (! handler)
-      ::abort();
-    handler ();
-    // FIXME: Replace std namespace when GCCv5+ C++ headers are installed
-    p = (void *) ::malloc (sz);
-  }
+    if (sz == 0)
+        sz = 1;
+    p      = (void *) malloc(sz);
+    while (p == 0) {
+        std::new_handler handler = __new_handler;
+        if (!handler)
+            ::abort();
+        handler();
+        // FIXME: Replace std namespace when GCCv5+ C++ headers are installed
+        p                        = (void *) ::malloc(sz);
+    }
 
-return p;
+    return p;
 }
 
-void*
-operator new[] (std::size_t sz)
-{
-return ::operator new(sz);
+void *
+operator new[] (std::size_t sz) {
+    return ::operator new(sz);
 }
 
 void
-operator delete(void* ptr)
-{
-if (ptr)
-  // FIXME: Replace std namespace when GCCv5+ C++ headers are installed
-  ::free(ptr);
+operator delete (void *ptr) {
+    if (ptr)
+        // FIXME: Replace std namespace when GCCv5+ C++ headers are installed
+        ::free(ptr);
 }
 
-new_handler
-std::set_new_handler (new_handler handler)
-{
-new_handler prev_handler = __new_handler;
-__new_handler = handler;
-return prev_handler;
+#if __GNUC__ >= 5
+void
+operator delete (void *ptr, std::size_t size) {
+    operator delete(ptr);
+}
+#endif
+
+std::new_handler std::set_new_handler (std::new_handler handler) {
+    std::new_handler prev_handler = __new_handler;
+    __new_handler = handler;
+    return prev_handler;
 }
