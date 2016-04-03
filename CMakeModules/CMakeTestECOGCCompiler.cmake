76c3ddf1dde6ae8b42951ac4c20af423bcc5708a
David Zemon <david@zemon.name>
2016-04-03 11:19:23 -0500

    [#82] Remove CMakeModules in preparation for git submodule

--- CMakeModules/CMakeTestECOGCCompiler.cmake
+++
@@ -1,5 +0,0 @@
-
-# The compiler configuration was forced by the user.
-# Assume the user has configured all compiler information.
-set(CMAKE_ECOGC_COMPILER_WORKS TRUE)
-return()
--- CMakeModules/CMakeTestSPIN2DATCompiler.cmake
+++
@@ -1,5 +0,0 @@
-
-# The compiler configuration was forced by the user.
-# Assume the user has configured all compiler information.
-set(CMAKE_SPIN2DAT_COMPILER_WORKS TRUE)
-return()
