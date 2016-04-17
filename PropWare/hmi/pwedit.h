06d2441b9acff0e40f771a9a6d065f29622f7893
David Zemon <david@zemon.name>
2016-04-17 11:16:09 -0500

    [#26] Full movement control working

--- PropWare/hmi/pwedit.h
+++ PropWare/hmi/pwedit.h
@@ -116,6 +116,12 @@ class PWEdit {
                         case 'k':
                             this->move_selection(UP);
                             break;
+                        case 'g':
+                            this->to_file_start();
+                            break;
+                        case 'G':
+                            this->to_file_end();
+                            break;
                     }
                 } while (EXIT_CHAR != c);
                 this->m_file->close();
@@ -232,32 +238,37 @@ class PWEdit {
         }
 
         void display_file () {
-            this->display_file_from_line(0);
+            this->display_file_from(0, 0);
 
             this->move_cursor(1, 1);
             this->m_termRow            = 1;
             this->m_termColumn         = 1;
-            this->m_selectedLineInFile = this->m_firstLine;
+            this->m_selectedLineNumber = this->m_firstLineDisplayed;
+            this->m_selectedLine       = this->m_lines.begin();
         }
 
-        void display_file_from_line (const unsigned int lineNumber) {
+        void display_file_from (const unsigned int startingLineNumber, const unsigned int startingColumnNumber) {
             auto lineIterator = this->m_lines.cbegin();
 
-            unsigned int i = lineNumber;
+            unsigned int i = startingLineNumber;
             while (i--)
                 ++lineIterator;
 
             for (uint8_t row = 1; row <= this->m_rows; ++row) {
+                const uint16_t charactersInLine = (*lineIterator)->get_size();
+
                 this->move_cursor(row, 1);
                 uint8_t column;
-                for (column = 0; column < this->m_columns && column < (*lineIterator)->get_size(); ++column)
-                    *this->m_printer << (*lineIterator)->to_string()[column];
+                for (column = 0;
+                     column < this->m_columns && (column + startingColumnNumber) < charactersInLine;
+                     ++column)
+                    *this->m_printer << (*lineIterator)->to_string()[column + startingColumnNumber];
                 while (column++ < this->m_columns)
                     *this->m_printer << ' ';
                 ++lineIterator;
             }
 
-            this->m_firstLine = lineNumber;
+            this->m_firstLineDisplayed = startingLineNumber;
         }
 
         void clear (const bool writeSpaces = true) const {
@@ -293,39 +304,131 @@ class PWEdit {
         }
 
         void move_down () {
-            if ((this->m_lines.size() - 1) == this->m_selectedLineInFile)
+            if ((this->m_lines.size() - 1) <= this->m_selectedLineNumber)
                 *this->m_printer << BELL;
             else {
-                const unsigned int lastLine = this->m_firstLine + this->m_rows;
+                const bool         redrawNecessary = this->trim_column_selection_to_fit(DOWN);
+                const unsigned int lastLineInFile  = this->m_firstLineDisplayed + this->m_rows;
                 if (PADDING > (this->m_rows - this->m_termRow)
-                        && this->m_lines.size() > lastLine) {
-                    this->display_file_from_line(++this->m_firstLine);
+                        && this->m_lines.size() > lastLineInFile) {
+                    this->display_file_from(++this->m_firstLineDisplayed, this->m_firstColumnDisplayed);
                     this->move_cursor(this->m_termRow, this->m_termColumn);
                 } else {
+                    if (redrawNecessary)
+                        this->display_file_from(this->m_firstLineDisplayed, this->m_firstColumnDisplayed);
                     this->move_cursor(++this->m_termRow, this->m_termColumn);
                 }
-                ++this->m_selectedLineInFile;
+                ++this->m_selectedLineNumber;
+                ++this->m_selectedLine;
             }
         }
 
         void move_up () {
-            if (!this->m_selectedLineInFile)
+            if (!this->m_selectedLineNumber)
                 *this->m_printer << BELL;
             else {
-                if (PADDING >= this->m_termRow && this->m_firstLine) {
-                    this->display_file_from_line(--this->m_firstLine);
+                const bool redrawNecessary = this->trim_column_selection_to_fit(UP);
+                if (PADDING >= this->m_termRow && this->m_firstLineDisplayed) {
+                    this->display_file_from(--this->m_firstLineDisplayed, this->m_firstColumnDisplayed);
                     this->move_cursor(this->m_termRow, this->m_termColumn);
                 } else {
+                    if (redrawNecessary)
+                        this->display_file_from(this->m_firstLineDisplayed, this->m_firstColumnDisplayed);
                     this->move_cursor(--this->m_termRow, this->m_termColumn);
                 }
-                --this->m_selectedLineInFile;
+                --this->m_selectedLineNumber;
+                --this->m_selectedLine;
             }
         }
 
         void move_right () {
+            if (((*this->m_selectedLine)->get_size() - 1) <= this->m_selectedColumnInLine) {
+                *this->m_printer << BELL;
+            } else {
+                const unsigned int lastColumnInLine = this->m_firstColumnDisplayed + this->m_columns;
+                if (PADDING > (this->m_columns - this->m_termColumn)
+                        && (*this->m_selectedLine)->get_size() > lastColumnInLine) {
+                    this->display_file_from(this->m_firstLineDisplayed, ++this->m_firstColumnDisplayed);
+                    this->move_cursor(this->m_termRow, this->m_termColumn);
+                } else
+                    this->move_cursor(this->m_termRow, ++this->m_termColumn);
+                ++this->m_selectedColumnInLine;
+            }
         }
 
         void move_left () {
+            if (!this->m_selectedColumnInLine)
+                *this->m_printer << BELL;
+            else {
+                if (PADDING >= this->m_termColumn && this->m_firstColumnDisplayed) {
+                    this->display_file_from(this->m_firstLineDisplayed, --this->m_firstColumnDisplayed);
+                    this->move_cursor(this->m_termRow, this->m_termColumn);
+                } else
+                    this->move_cursor(this->m_termRow, --this->m_termColumn);
+                --this->m_selectedColumnInLine;
+            }
+        }
+
+        bool trim_column_selection_to_fit (const Direction direction) {
+            auto tempIterator = m_selectedLine;
+            switch (direction) {
+                case UP:
+                    --tempIterator;
+                    break;
+                case DOWN:
+                    ++tempIterator;
+                    break;
+                default:
+                    return false;
+            }
+
+            bool               redrawNecessary = false;
+            const unsigned int lineLength      = (*tempIterator)->get_size();
+
+            const bool lineShorterThanSelection = lineLength <= this->m_selectedColumnInLine;
+            if (lineShorterThanSelection) {
+                this->m_selectedColumnInLine = lineLength - 1;
+                if (this->m_firstColumnDisplayed > this->m_selectedColumnInLine) {
+                    this->m_firstColumnDisplayed = this->m_selectedColumnInLine;
+                    this->m_termColumn           = 1;
+                    redrawNecessary = true;
+                } else
+                    this->m_termColumn = (uint8_t) (this->m_selectedColumnInLine - this->m_firstColumnDisplayed + 1);
+            }
+
+            return redrawNecessary;
+        }
+
+        void to_file_start () {
+            this->m_firstLineDisplayed   = 0;
+            this->m_firstColumnDisplayed = 0;
+            this->m_selectedLineNumber   = 0;
+            this->m_selectedLine         = this->m_lines.cbegin();
+            this->m_selectedColumnInLine = 0;
+            this->m_termRow              = 1;
+            this->m_termColumn           = 1;
+            this->display_file_from(0, 0);
+            this->move_cursor(this->m_termRow, this->m_termColumn);
+        }
+
+        void to_file_end () {
+            this->m_firstLineDisplayed = this->m_lines.size() - this->m_rows;
+            uint8_t lastColumn;
+            if (this->m_lines.back()->get_size() > this->m_columns) {
+                this->m_firstColumnDisplayed = this->m_lines.back()->get_size() - this->m_columns;
+                lastColumn = (uint8_t) (this->m_columns - 1);
+            } else {
+                this->m_firstColumnDisplayed = 0;
+                lastColumn = (uint8_t) (this->m_lines.back()->get_size());
+            }
+            this->m_selectedLine         = this->m_lines.cend();
+            --this->m_selectedLine; // Need to point to the last line, not the "end" which is one after the last
+            this->m_selectedLineNumber = this->m_lines.size() - 1;
+            this->m_selectedColumnInLine = (unsigned int) ((*this->m_selectedLine)->get_size() - 1);
+            this->m_termRow            = this->m_rows;
+            this->m_termColumn         = lastColumn;
+            this->display_file_from(this->m_firstLineDisplayed, this->m_firstColumnDisplayed);
+            this->move_cursor(this->m_termRow, this->m_termColumn);
         }
 
     protected:
@@ -340,18 +443,22 @@ class PWEdit {
         /** Total rows on screen */
         uint8_t m_rows;
 
-        /** Current cursor row */
+        /** Current cursor row (1-indexed) */
         uint8_t m_termRow;
-        /** Current cursor column */
+        /** Current cursor column (1-indexed) */
         uint8_t m_termColumn;
 
-        /** Current line in file selected */
-        unsigned int m_selectedLineInFile;
-        /** Current column in file selected */
+        std::list<StringBuilder *>::const_iterator m_selectedLine;
+
+        /** Index of currently selected line in the file (0-indexed) */
+        unsigned int m_selectedLineNumber;
+        /** Index of currently selected column in the line (0-indexed) */
         unsigned int m_selectedColumnInLine;
 
-        /** First line displayed */
-        unsigned int m_firstLine;
+        /** First visible line of the file (0-indexed) */
+        unsigned int m_firstLineDisplayed;
+        /** First visible column of the line displayed (0-indexed) */
+        unsigned int m_firstColumnDisplayed;
 };
 
 }
