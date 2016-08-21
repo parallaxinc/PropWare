670373522c4a6ab6d78e9c1116b8d10426be3448
David Zemon <david@zemon.name>
2016-08-21 14:32:21 -0500

    Add documentation for FDS demo

--- Examples/PropWare_FullDuplexSerial/FullDuplexSerial_Demo.cpp
+++ Examples/PropWare_FullDuplexSerial/FullDuplexSerial_Demo.cpp
@@ -27,38 +27,54 @@
 #include <PropWare/hmi/input/scanner.h>
 #include <PropWare/serial/uart/fullduplexserial.h>
 
+void runScannerDemo (PropWare::Printer &queuePrinter, PropWare::Scanner &queueScanner);
+
 bool isAnswerNo (char const userInput[]);
 
+/**
+ * @example FullDuplexSerial_Demo.cpp
+ *
+ * Use the tried and true FullDuplexSerial driver to communicate asynchronously with a user. Formatting is done via
+ * PropWare's standard PropWare::Printer and PropWare::Scanner classes, unlike the original FullDuplexSerial.spin
+ * object.
+ *
+ * @include Examples/PropWare_FullDuplexSerial/CMakeLists.txt
+ */
 int main () {
     PropWare::FullDuplexSerial serial;
     serial.start();
 
     PropWare::Printer printer(serial);
-    PropWare::Scanner scanner(serial);
+    PropWare::Scanner scanner(serial, &printer);
 
+    // This should look very familiar - it is the same code as Examples/PropWare_Scanner/Scanner_Demo.cpp
+    runScannerDemo(printer, scanner);
+
+    while (1);
+}
+
+void runScannerDemo (PropWare::Printer &queuePrinter, PropWare::Scanner &queueScanner) {
     // A nice big buffer that can hold up to 63 characters from the user (the
     // 64th is used by the null-terminator)
     char         userInput[64];
     char         name[64];
     unsigned int age;
 
-    printer << "Hello! I'd like to teach you how to use PropWare to read from the terminal!\n";
+    queuePrinter << "Hello! I'd like to teach you how to use PropWare to read from the terminal!\n";
 
     do {
-        printer << "First, what's your name?\n>>> ";
-        scanner >> name;
+        queuePrinter << "First, what's your name?\n>>> ";
+        queueScanner >> name;
 
-        printer << "And how old are you?\n>>> ";
-        scanner >> age;
+        queuePrinter << "And how old are you?\n>>> ";
+        queueScanner >> age;
 
-        printer << "Is your name " << name << " and are you " << age << " years old?\n>>> ";
-        scanner.input_prompt("", "Please enter either 'yes' or 'no' (y/n)\n>>> ", userInput, sizeof(userInput),
-                             YES_NO_COMP);
+        queuePrinter << "Is your name " << name << " and are you " << age << " years old?\n>>> ";
+        queueScanner.input_prompt("", "Please enter either 'yes' or 'no' (y/n)\n>>> ", userInput, sizeof(userInput),
+                                  YES_NO_COMP);
     } while (isAnswerNo(userInput));
 
-    printer << "Hello, " << name << "!\n";
-
-    while (1);
+    queuePrinter << "Hello, " << name << "!\n";
 }
 
 bool isAnswerNo (char const userInput[]) {
