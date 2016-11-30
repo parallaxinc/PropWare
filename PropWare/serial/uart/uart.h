b2b39c92dea4a7df09b91c9255a6b4c7e8163a43
Markus Ebner <seijikun@users.noreply.github.com>
2016-11-30 14:09:58 +0100

    Add a new Timeout-Error for the UART

--- PropWare/serial/uart/uart.h
+++ PropWare/serial/uart/uart.h
@@ -107,6 +107,7 @@ class UART {
             /** The requested data width is not between 1 and 16 (inclusive) */    INVALID_DATA_WIDTH,
             /** The requested stop bit width is not between 1 and 14 (inclusive) */INVALID_STOP_BIT_WIDTH,
             /** Null pointer was passed as an argument */                          NULL_POINTER,
+            /** Reading from the serial connection timed out */                    TIMEOUT_ERROR,
             /** Last error code used by PropWare::UART */                          END_ERROR     = UART::NULL_POINTER
         } ErrorCode;
 
