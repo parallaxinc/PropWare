17ee3afaa219e666072e5162a75ecf5d101a0d66
David Zemon <david@zemon.name>
2019-01-18 22:05:08 -0600

    Update URLs for new CMake version

--- DownloadCMakeBinaries.cmake
+++ DownloadCMakeBinaries.cmake
@@ -1,6 +1,4 @@
-set(CMAKE_MAJOR_VERSION 3)
-set(CMAKE_MINOR_VERSION 13)
-set(CMAKE_PATCH_VERSION 3)
+set(CMAKE_DOWNLOAD_VERSION 3.13.3)
 set(CUSTOM_LINUX_CMAKE_INSTALL_DIR LinuxCMake)
 set(CUSTOM_WIN32_CMAKE_INSTALL_DIR WinCMake)
 set(CUSTOM_OSX_CMAKE_INSTALL_DIR   OsxCMake)
@@ -9,7 +7,7 @@ set(CUSTOM_PI2_CMAKE_INSTALL_DIR   Pi2CMake)
 if (PACKAGE_LINUX)
     externalproject_add(CMake
         PREFIX CMake-src
-        URL https://www.cmake.org/files/v${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}/cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION}-Linux-x86_64.tar.gz
+        URL https://github.com/Kitware/CMake/releases/download/v${CMAKE_DOWNLOAD_VERSION}/cmake-${CMAKE_DOWNLOAD_VERSION}-Linux-x86_64.tar.gz
         CONFIGURE_COMMAND ${CMAKE_COMMAND} -E echo_append
         BUILD_COMMAND ${CMAKE_COMMAND} -E echo_append
         INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR> ${CMAKE_BINARY_DIR}/${CUSTOM_LINUX_CMAKE_INSTALL_DIR})
@@ -18,7 +16,7 @@ endif ()
 if (PACKAGE_WIN32)
     externalproject_add(WinCMake
         PREFIX WinCMake-src
-        URL https://www.cmake.org/files/v${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}/cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION}-win32-x86.zip
+        URL https://github.com/Kitware/CMake/releases/download/v${CMAKE_DOWNLOAD_VERSION}/cmake-${CMAKE_DOWNLOAD_VERSION}-win32-x86.zip
         CONFIGURE_COMMAND ${CMAKE_COMMAND} -E echo_append
         BUILD_COMMAND ${CMAKE_COMMAND} -E echo_append
         INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR> ${CMAKE_BINARY_DIR}/${CUSTOM_WIN32_CMAKE_INSTALL_DIR})
@@ -27,7 +25,7 @@ endif ()
 if (PACKAGE_OSX)
     externalproject_add(OSXCMake
         PREFIX OSXCMake-src
-        URL https://www.cmake.org/files/v${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}/cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION}-Darwin-x86_64.tar.gz
+        URL https://github.com/Kitware/CMake/releases/download/v${CMAKE_DOWNLOAD_VERSION}/cmake-${CMAKE_DOWNLOAD_VERSION}-Darwin-x86_64.tar.gz
         CONFIGURE_COMMAND ${CMAKE_COMMAND} -E echo_append
         BUILD_COMMAND ${CMAKE_COMMAND} -E echo_append
         INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR> ${CMAKE_BINARY_DIR}/${CUSTOM_OSX_CMAKE_INSTALL_DIR})
